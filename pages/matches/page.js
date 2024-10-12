3876eb4f8e11bab3606114cca815405430404eb2
Amy Lee <amy.jeeyoon.lee@gmail.com>
2024-10-12 13:50:59 -0700

    basic page (fix front end)

--- pages/matches/page.js
+++ pages/matches/page.js
@@ -1,8 +1,40 @@
 'use client'
 
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore"
+import { db } from "@/firebase"
+import React, { useState, useEffect } from "react";
 
 export default function Matches() {
     const {user, isAuthenticated, isLoading} = useAuth0();
+    const [matches, setMatches] = useState([]);
+    const router = useRouter();
+
+    useEffect(() => {
+        async function getMatches() {
+            if (!user)  return;
+            const docRef = doc(collection(db, 'users'), user.id);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                const collections = docSnap.data().matches || [];
+                setMatches(collections);
+            } else {
+                await setDoc(docRef, {matches: []});
+            }
+        }
+
+        getMatches()
+    }, [user])
+
+    if (!isLoaded || !isSignedIn) {
+        return <div></div>
+    }
+
+    return (
+        <div>
+            <Image src="/logos/text.png" alt="Mantine logo" />
+        </div>
+    );
 }
